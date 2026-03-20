d3133559c11ed7265bf1c2318d06b3a0a9d7f27a
Olli Numminen <olli.numminen@tuni.fi>
2026-03-20 21:09:13 +0200

    Start sketching initiative calculator

---
+++ initiative.py
@@ -0,0 +1,3 @@
+from enum import Enum
+
+def calculate_initiative(previous_turn_initiative):
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from initiative import calculate_initiative
+
+NUMBER_OF_ROUNDS = 3
+
 def main():
     print("Calculator opened!")
+    for round in NUMBER_OF_ROUNDS:
+        scenario_based_initiative = input(f"Give scenario based initiative for round {round}, leave empty if determined by previous round") 
+        previous_initiative = None
+        current_initiative = None
+    if scenario_based_initiative is not None #
+    calculate_initiative(Round)
+
 
     
 if __name__ == "__main__":
