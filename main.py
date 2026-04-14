35323190bc4c0e6d1fe76e840483ca0c4ad4aa48
Olli Numminen <olli.numminen@tuni.fi>
2026-04-14 08:37:43 +0300

    Improve robusteness

--- main.py
+++ main.py
@@ -17,14 +17,22 @@ def main():
         print(f"Start of round {round_num}")
         state.round = round_num
 
-        if state.round == 1 and SCENARIO_INITIATIVES[1] is None:
+        ########################################################################
+        # Initiative
+        ########################################################################
+        if state.round == 1 and SCENARIO_INITIATIVES.get(1) is None:
             raise ValueError("The initiative must be preset for the first round!")
         if SCENARIO_INITIATIVES[state.round] is None:
             state.initiative = calculate_initiative(state.prev_initiative, state.prev_vp_blue, state.prev_vp_red)
         else:
-            state.initiative = SCENARIO_INITIATIVES[state.round]
+            state.initiative = SCENARIO_INITIATIVES.get(state.round)
 
         print(f"Initiative side is: {state.initiative}")
+
+
+        ########################################################################
+        # 
+        ########################################################################
 
         # Last thing of the round, update previous round values with current ones
         # and reset the current round counters
